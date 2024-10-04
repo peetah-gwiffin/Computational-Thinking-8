039969625c8ceddbffe3c20a453ac451e511b167
peetah-gwiffin <petekicska@seattleacademy.org>
2024-10-04 15:39:16 +0000

    test

--- Projects/hsaep.py
+++ Projects/hsaep.py
@@ -3,9 +3,15 @@ import turtle
 
 t = turtle.Turtle()
 
+t.penup()
+
 t.goto(100,0)
+t.pendown()
 t.color("red")
 
-for i in range(100000000000000000000000):
+
+
+for i in range(100000000000000000000000000000000000000000000000000000000000000000000000000000000):
     t.forward(1)
     t.left(1.1)
+    
